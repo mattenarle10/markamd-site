79462a6a405d9643c2ab97ef7b3d107ff03809b4
mattenarle10 <enarlem10@gmail.com>
2026-06-02 00:15:46 +0800

    refactor(site): centralize icon exports

---
+++ src/components/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as AppleIcon } from "./AppleIcon.astro";
+export { default as CheckIcon } from "./CheckIcon.astro";
+export { default as CopyIcon } from "./CopyIcon.astro";
+export { default as DownloadIcon } from "./DownloadIcon.astro";
+export { default as GithubIcon } from "./GithubIcon.astro";
+export { default as LinuxIcon } from "./LinuxIcon.astro";
+export { default as WindowsIcon } from "./WindowsIcon.astro";
